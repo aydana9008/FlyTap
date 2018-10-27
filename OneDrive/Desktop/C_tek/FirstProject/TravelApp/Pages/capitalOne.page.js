b9bf5e69120aa8615f9efa90f5e3591fabe50d73
Aydana9008 <aydana_9008@yahoo.com>
2018-10-27 14:49:48 -0400

    POM file

---
+++ OneDrive/Desktop/C_tek/FirstProject/TravelApp/Pages/capitalOne.page.js
@@ -0,0 +1,8 @@
+var CapitalOnePage = function(){
+this.CreditCardsButton = $(".site-header__primary-nav>button:nth-child(1)");
+this.CheckingAndSavingButton = element(by.buttonText("Checking & Savings"));
+this.AutoLoansButton = $("#auto");
+this.BusinessButton = element(by.buttonText("Business"));
+this.CommercialButton = element(by.buttonText("Commercial"));
+}
+module.exports = new CapitalOnePage();
