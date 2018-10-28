ef6e2f0d49d19dc7c5f269ebfdd161ed11137301
Aydana9008 <aydana_9008@yahoo.com>
2018-10-28 16:40:01 -0400

    CapOne Configuration file

---
+++ OneDrive/Desktop/C_tek/capitalOneProject/Config/conf.js
@@ -0,0 +1,48 @@
+var HtmlReporter = require('protractor-beautiful-reporter');
+var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+
+exports.config = {
+  //  seleniumAddress: 'http://localhost:4444/wd/hub',
+    directConnect : true,
+  
+   multiCapabilities: [
+      {browserName: 'chrome' },
+      {browserName: 'firefox'},
+      //{browserName: "internet explorer"}
+   ],
+  
+  specs: ['../Tests/capitalOneHomePageHeader.spec.js'], 
+
+//   suites:{
+//   smoke: ["../Tests/BankManagerSimple.spec.js", "../Tests/alerts.spec.js"],
+//   regression: ["../Tests/*.spec.js"]
+//   },
+
+onPrepare: function () {
+    browser.driver.manage().window().maximize();
+
+    jasmine.getEnv().addReporter(new SpecReporter({
+        displayFailuresSummary: true,
+        displayFailuredSpec: true,
+        displaySuiteNumber: true,
+        displaySpecDuration: true,
+        showstack: false
+      }));
+      // Add a screenshot reporter and store screenshots to `/tmp/screenshots`:
+      jasmine.getEnv().addReporter(new HtmlReporter({
+        baseDirectory: '../report/screenshots',
+        preserveDirectory: false,
+        screenshotsSubfolder: 'images',
+         jsonsSubfolder: 'jsons',
+         docName: 'CapitalOne-Report.html'
+     }).getJasmine2Reporter());
+  
+},
+    
+    jasmineNodeOpts: {
+        showColors: true, 
+        defaultTimeoutInterval: 300000,    
+        print: function() {}
+        
+}
+};
