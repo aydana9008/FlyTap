ee8dee319e3e2357fe50bf49ddd02ad70c5a4754
Aydana9008 <aydana_9008@yahoo.com>
2018-10-30 22:06:32 -0400

    Deleting all separate files

--- OneDrive/Desktop/C_tek/FirstProject/TravelApp/Pages/capitalOne.page.js
+++
@@ -1,8 +0,0 @@
-var CapitalOnePage = function(){
-this.CreditCardsButton = $(".site-header__primary-nav>button:nth-child(1)");
-this.CheckingAndSavingButton = element(by.buttonText("Checking & Savings"));
-this.AutoLoansButton = $("#auto");
-this.BusinessButton = element(by.buttonText("Business"));
-this.CommercialButton = element(by.buttonText("Commercial"));
-}
-module.exports = new CapitalOnePage();
